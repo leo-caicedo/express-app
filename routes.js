4e7f57893a1ddf14424a7381f389431435302af3
Leonardo Caicedo <devleonardocaicedo@gmail.com>
2021-09-08 21:51:06 -0500

    main

---
+++ routes.js
@@ -0,0 +1,16 @@
+const { Router } = require("express");
+const router = Router();
+
+router.get("/", (req, res) => {
+  res.json({ message: "hello friend" });
+});
+
+router.get("/home", (req, res) => {
+  res.json({ message: "welcome to home" });
+});
+
+router.get("/about", (req, res) => {
+  res.json({ message: "welcome to about" });
+});
+
+module.exports = router;
